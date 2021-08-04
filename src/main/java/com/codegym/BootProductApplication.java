9feb8a4b7ca2a81a044f4a1250d0f9fe4d46c3e9
thaonoucamp <dangxuanthaoht@gmail.com>
2021-08-04 10:15:34 +0700

    init

---
+++ src/main/java/com/codegym/BootProductApplication.java
@@ -0,0 +1,13 @@
+package com.codegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootProductApplication.class, args);
+    }
+
+}
